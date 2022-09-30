9f30c0b579339418752186fa0ece6d6261ccf2bb
tanjakatariina <tanja.k.pyykonen@gmail.com>
2022-09-30 10:59:23 +0300

    Added list and add components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
