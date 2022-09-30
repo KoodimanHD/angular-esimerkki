3ed5a5aed7e7a5809b7edfbc43806b062d6375db
tanjakatariina <tanja.k.pyykonen@gmail.com>
2022-09-30 16:59:17 +0300

    Cleaned up files.

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -1,29 +1,25 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { map, catchError, tap } from 'rxjs/operators';
+import {HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Event } from './event';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class EventService {
   url = "http://localhost:3000/events";
 
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) {
-
-  }
-
-  /** GET heroes from the server */
+  /**
+   * Get events from the server
+   */
   getEvents(): Observable<Event[]> {
     return this.http.get<Event[]>(this.url)
       .pipe(
         tap(_ => console.log('Events get'))
       );
   }
-
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import {Event} from "../event";
-import {catchError, map, Observable, throwError} from "rxjs";
 import {EventService} from "../event.service";
 
 @Component({
@@ -9,30 +7,19 @@ import {EventService} from "../event.service";
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
+
 export class ListComponent implements OnInit {
-  events : Event[] = []
+  events: Event[] = []
   Object = Object
 
-  constructor(private eventService : EventService) {}
+  constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
-  this.getEvents()
-    if(this.events[0].name) {console.log("ON!")}
-    console.log("Eventit " + this.events)
+    this.getEvents()
   }
+
   getEvents(): void {
     this.eventService.getEvents()
       .subscribe(events => this.events = events);
-    console.log(this.events)
   }
-
-/*
-  getKeys(events: Event[]) : void {
-    console.log(events[0])
-      this.keys = Object.keys(events[0]) as string[]
-      this.keys.forEach(function (value) {
-        console.log("KEY:" + value)
-      })
-  }
- */
 }
